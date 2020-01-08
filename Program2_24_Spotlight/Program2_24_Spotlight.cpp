e9e9f23ca3372653bccd07bf1d555b15bd35d8e7
lilitkhamalyan <lilitkhamalyan@gmail.com>
2020-01-07 23:14:12 -0500

    Calculates and displays the rightmost digit of the number.

--- Program2_24_Spotlight/Program2_24_Spotlight.cpp
+++ Program2_24_Spotlight/Program2_24_Spotlight.cpp
@@ -1,21 +1,16 @@
-// Program2_24_Spotlight.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// name: Lilit Khamalyan
+// date: 1/07/2020
+// Program 2-24: This program extracts the righmost digit of a number.
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+     int number = 125;
+     int rightMost = number % 10;
+     // Display the result
+     cout << "The rightmost digit in " << number << " is " << rightMost << endl;
+     system("pause");
+     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
